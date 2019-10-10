b32ec045e06bb7d3800a37d4baf48afe995e30bd
Tyler Gilchrist <tyler668@gmail.com>
2019-10-10 19:49:34 -0400

    mvp

--- models/recipe-model.js
+++ models/recipe-model.js
@@ -10,14 +10,6 @@ function getRecipes() {
     return db('recipes');
 }
 
-// function getShoppingList(recipe_id) {
-//     return db('ingredients')
-//         .join('ingregients', 'ingredients.id', '=', 'link.ingredient_id')
-    
-//         .where({ recipe_id })
-//         .orderBy('id')
-// }
-
 function getShoppingList(recipeId) {
     return db('recipes_ingredients')
     .join('ingredients', 'recipes_ingredients.ingredient_id', '=', 'ingredients.id')
